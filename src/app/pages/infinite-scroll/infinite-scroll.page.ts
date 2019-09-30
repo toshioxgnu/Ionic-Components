323f723ff70c28a16c163c82ba70dc9afdbf209a
toshioxgnu <jozho.g@gmail.com>
2019-09-30 11:18:52 -0300

    Implementando Infinite Scroll

---
+++ src/app/pages/infinite-scroll/infinite-scroll.page.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { TIMEOUT } from 'dns';
+import { IonInfiniteScroll } from '@ionic/angular';
+
+@Component({
+  selector: 'app-infinite-scroll',
+  templateUrl: './infinite-scroll.page.html',
+  styleUrls: ['./infinite-scroll.page.scss'],
+})
+export class InfiniteScrollPage implements OnInit {
+
+  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
+
+  data: any[] = Array(20);
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  loadData(event){
+    console.log('Cargando siguientes');
+
+    setTimeout(() => {
+      if (this.data.length > 50) {
+        event.target.complete();
+        this.infiniteScroll.disabled = true;
+        return;
+      }
+
+      const newArr = Array(20);
+      this.data.push( ...newArr);
+      event.target.complete();
+    }, 1000);
+  }
+
+}
--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -52,6 +52,11 @@ export class InicioPage implements OnInit {
     icon: 'ios-grid',
     name: 'Grid',
     redirectTo: '/grid'
+   },
+   {
+    icon: 'infinite',
+    name: 'Infinite Scroll',
+    redirectTo: '/infinite-scroll'
    }
   ];
   constructor() { }
